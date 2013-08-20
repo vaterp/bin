38cc2154084b45b7791ae0153794f96a8fb64e63
vaterp-rPP1 <brian.snyder@gmail.com>
2013-08-20 16:07:07 -0400

    Updates to bhpb.sh

--- bhpb.sh
+++ bhpb.sh
@@ -1,14 +1,19 @@
-unsetopt autopushd
-pushd $h
-cd samnc && make
-cd ../sarouter && make
-cd ../sana && make
-cd ../sada && make
-cd ../sarmt && make
+#!/bin/zsh
+cd $h
+echo "Building samnc"
+cd samnc && make > /dev/null
+echo "Building sarouter"
+cd ../sarouter && make > /dev/null
+echo "Building sana"
+cd ../sana && make > /dev/null
+echo "Building sada"
+cd ../sada && make > /dev/null
+echo "Building sarmt"
+cd ../sarmt && make > /dev/null
 cd ..
  find . -type f -name sada  -ls | cut -d' ' -f3,11-
  find . -type f -name sana  -ls | cut -d' ' -f3,11-
  find . -type f -name sarouter  -ls | cut -d' ' -f3,11-
  find . -type f -name sarmt  -ls | cut -d' ' -f3,11-
-popd
-setopt autopushd
+ echo -n "Current date is: " 
+ date
