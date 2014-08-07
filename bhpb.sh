a603ad76b0653e442db80dddcc9dab6d9501c93d
vaterp-rPP1 <brian.snyder@gmail.com>
2014-08-07 14:07:31 -0400

    Updates

--- bhpb.sh
+++ bhpb.sh
@@ -12,10 +12,18 @@ echo "Building sada"
 cd ../sada && make > /dev/null
 echo "Building sarmt"
 cd ../sarmt && make > /dev/null
+echo "Building iCon"
+cd ../consolidator && make > /dev/null
+echo "Building iac"
+cd ../iac && make > /dev/null
+
 cd ..
+echo "\n\n\n\nNew Binaries:"
  find . -type f -name sada  -ls | cut -d' ' -f3,11-
  find . -type f -name sana  -ls | cut -d' ' -f3,11-
  find . -type f -name sarouter  -ls | cut -d' ' -f3,11-
  find . -type f -name sarmt  -ls | cut -d' ' -f3,11-
- echo -n "Current date is: " 
- date
+ find . -type f -name iCon  -ls | cut -d' ' -f3,11-
+ find . -type f -name iac  -ls | cut -d' ' -f3,11-
+echo -n "Current date is: " 
+date
