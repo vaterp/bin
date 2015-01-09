4ef1dfe0f4c71fc70673e1c9e0788ff558154612
vaterp-rhub <brian.snyder@gmail.com>
2015-01-09 10:52:29 -0500

    Some updates

--- loadnewmdmpkg.sh
+++ loadnewmdmpkg.sh
@@ -1,11 +1,11 @@
-remversion="13.0.51.0"
+remversion="14.0.50.0"
 wget http://releases.eng.idirect.net/IDS-$remversion-latest/IDS.BUILD.NAME
-wget http://releases.eng.idirect.net/IDS-$remversion-latest/pkg/evo_x5_rmt-$remversion.pkg
+wget http://releases.eng.idirect.net/IDS-$remversion-latest/pkg/evo_x7_rmt-$remversion.pkg
 wget http://releases.eng.idirect.net/IDS-$remversion-latest/pkg/evo_x1_indoor_rmt-$remversion.pkg
 x=`cat IDS.BUILD.NAME | cut -d'-' -f3-`
 
 mv evo_x1_indoor_rmt-$remversion.pkg /home/nms/cfg/image_sets/evo_x1_indoor_package/evo_x1_indoor_rmt-$remversion-$x.pkg 
-mv evo_x5_rmt-$remversion.pkg /home/nms/cfg/image_sets/evo_x5_remote_package/evo_x5_rmt-$remversion-$x.pkg
+mv evo_x7_rmt-$remversion.pkg /home/nms/cfg/image_sets/evo_x7_package/evo_x7_rmt-$remversion-$x.pkg
 
 echo "Build Number Just Installed is:"
 cat IDS.BUILD.NAME
--- update.sh
+++ update.sh
@@ -1,14 +1,13 @@
-for i in $(find . -maxdepth 1 -type d)
+for i in $(find . -maxdepth 1 -mindepth 1 -type d)
 do
-	if [ "$i" == "." ] 
-	then
-		continue
-	fi
-	
-	echo -e "\033[1;34mUpdating $i \033[0m"
-
 	cd $i
-	git pull | grep -v "Already up-to-date."
-	git b
+	git rev-parse --git-dir > /dev/null
+	if (($? -eq 0)); then
+		echo -e "\033[1;34mUpdating $i \033[0m"
+		#git pull | grep -v "Already up-to-date."
+		git b
+	else
+		echo -e "\034[1;34mNOT A GIT REPO $i \033[0m"
+	fi
 	cd ..
 done
